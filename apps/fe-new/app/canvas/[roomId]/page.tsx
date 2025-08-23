c8565d81e532d5da3f900f225c8cd732162de289
krrakshit <rkrkr365@gmail.com>
2025-08-23 19:37:06 +0530

    added more shapes and its type added classed and handling mouse events

---
+++ apps/fe-new/app/canvas/[roomId]/page.tsx
@@ -0,0 +1,12 @@
+import { Canvas } from "@/components/Canvas";
+
+export default async function CanvasPage({ params }: {
+    params: {
+        roomId: string
+    }
+}) {
+    const roomId = (await params).roomId;
+
+    return <Canvas roomId={roomId} />
+   
+}
